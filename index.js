5e8f19c0ecb57259d78d7165dcbca1f5d0098b1e
Aditya <105660945+AdiTheRipper@users.noreply.github.com>
2023-05-04 17:08:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -264,6 +264,6 @@ function openWhatsapp() {
     }
 
     let wTxtEncoded = encodeURI(wTxt);
-    window.open("https://wa.me/917428789065?text=" + wTxtEncoded);
+  window.open("https://wa.me/919511225483?text=" + wTxtEncoded);
   }
 }
